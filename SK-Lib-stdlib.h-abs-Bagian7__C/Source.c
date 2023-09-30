3d297574ea958019e703c18ff033fb8a30218a79
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-09-30 09:13:41 +0700

    Initial commit

---
+++ SK-Lib-stdlib.h-abs-Bagian7__C/Source.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <conio.h>
+
+/*
+    Source by Microsoft (https://docs.microsoft.com/en-us/cpp)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+    int ix = -4;
+    long lx = -41567L;
+    long long llx = -9876543210LL;
+    __int64 wx = -1;
+
+    // absolute 32 bit integer value
+    printf_s("The absolute value of %d is %d\n", ix, abs(ix));
+
+    // absolute long integer value
+    printf_s("The absolute value of %ld is %ld\n", lx, labs(lx));
+
+    // absolute long long integer value
+    printf_s("The absolute value of %lld is %lld\n", llx, llabs(llx));
+
+    // absolute 64 bit integer value
+    printf_s("The absolute value of 0x%.16I64x is 0x%.16I64x\n", wx, _abs64(wx));
+
+    // Integer error cases:
+    printf_s("Microsoft implementation-specific results:\n");
+    printf_s(" abs(INT_MIN) returns %d\n", abs(INT_MIN));
+    printf_s(" labs(LONG_MIN) returns %ld\n", labs(LONG_MIN));
+    printf_s(" llabs(LLONG_MIN) returns %lld\n", llabs(LLONG_MIN));
+    printf_s(" _abs64(_I64_MIN) returns 0x%.16I64x\n", _abs64(_I64_MIN));
+
+    _getch();
+    return 0;
+}
